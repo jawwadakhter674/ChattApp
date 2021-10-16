087448d526b4904a615efb730442f1bbf068ebb3
HamaSarbast-1997 <84290000+HamaSarbast-1997@users.noreply.github.com>
2021-10-16 23:08:43 +0300

    Auth Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie';
-import {ChanellListContainer, ChannelContainer} from './components';
+import {ChanellListContainer, ChannelContainer , Auth} from './components';
 import './App.css'
 
 const apiKey = 'rybud6mqjvyb';
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
+
 const App = () => {
+    if(!authToken) return <Auth/>
     return (
         <div className="app__wrapper">
 
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import signinImage from '../assets/signup.jpg';
+
+
+const Auth = () => {
+    const [isSignup, setSignup] = useState(true);
+
+    const handleChange = (e) => {
+       console.log(e.target.value)
+    }
+
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={() => { }}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input
+                                    name="fullName"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {/* this usename is not inside that isSignUo beciuse we want userName just inside signin */}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                name="username"
+                                type="text"
+                                placeholder="Username"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="password">Confirm Pssword</label>
+                                <input
+                                    name="password"
+                                    type="password"
+                                    placeholder="Confrm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
